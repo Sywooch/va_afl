996d6962a1a9b5e25bfaa8f0f19df980e94fa43c
Александр Шахмухаметов <diablus2012@gmail.com>
2015-09-20 00:58:42 +0500

    #2 fixed host parameter

--- config/params.php
+++ config/params.php
@@ -2,7 +2,7 @@
 
 return [
 	'adminEmail' => 'admin@example.com',
-	'ivao_login_url' => 'https://login.ivao.aero/index.php?url=http://bth.va-aeroflot.su/site/login',
-	'ivao_api_url' => 'https://login.ivao.aero/api.php?type=json&token='
+    'ivao_login_url' => 'https://login.ivao.aero/index.php?url=http://' . $_SERVER['HTTP_HOST'] . '/site/login',
+    'ivao_api_url' => 'https://login.ivao.aero/api.php?type=json&token='
 ];
 
