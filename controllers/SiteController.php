2d15595fb8213ad3aa3259e467d1f17d807783a4
Александр Шахмухаметов <diablus2012@gmail.com>
2015-12-31 00:21:54 +0500

    Исправлен косяк в контроллере

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -138,40 +138,4 @@ class SiteController extends Controller
         }
         $this->goBack();
     }
-
-    public function behaviors()
-    {
-        return [
-            'access' => [
-                'class' => AccessControl::className(),
-                'only' => ['logout'],
-                'rules' => [
-                    [
-                        'actions' => ['logout'],
-                        'allow' => true,
-                        'roles' => ['@'],
-                    ],
-                ],
-            ],
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'logout' => ['post'],
-                ],
-            ],
-        ];
-    }
-
-    public function actions()
-    {
-        return [
-            'error' => [
-                'class' => 'yii\web\ErrorAction',
-            ],
-            'captcha' => [
-                'class' => 'yii\captcha\CaptchaAction',
-                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
-            ],
-        ];
-    }
 }
