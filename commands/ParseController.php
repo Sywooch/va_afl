d0744a213636382d4626585add2d1bf95bea39fc
Александр Шахмухаметов <diablus2012@gmail.com>
2016-01-05 19:23:47 +0500

    #15 Дополнение трекера

--- commands/ParseController.php
+++ commands/ParseController.php
@@ -37,7 +37,9 @@ class ParseController extends Controller
     const WZ_ICAOALT2 = 42;
     const WZ_POB = 44;
     const WZ_HEADING = 45;
-
+    const WZ_SIMULATOR = 48;
+    const WZ_REMARKS = 29;
+    const WZ_ALTERNATE = 28;
     const MAX_DISTANCE_TO_SAVE_FLIGHT = 10;
     const HOLD_TIME = 900;
 
@@ -55,6 +57,12 @@ class ParseController extends Controller
      * @var array
      */
     private $ourpilots;
+
+    /**
+     * Массив с VID всех наших пилотов онлайн
+     * @var array
+     */
+    private $onlinepilotslist;
 
     /**
      * Главная функция, запускает остальные функции
@@ -100,7 +108,7 @@ class ParseController extends Controller
     }
 
     /**
-     * Валидирует полет. Проверяет, что он завершен в радиусе 10 морских миль от аэродрома назначения.
+     * Валидирует полет. Проверяет, что он завершен в радиусе MAX_DISTANCE от аэродрома назначения.
      * @param $flight
      * @return bool
      */
@@ -123,21 +131,15 @@ class ParseController extends Controller
     private function startFlight($booking)
     {
         $flight = new Flights();
-
         if ($booking->fleet_regnum) {
             $flight->fleet_regnum = $booking->fleet_regnum;
         }
-
         $flight->acf_type = $booking->aircraft_type;
         $flight->booking_id = $booking->id;
-
         $flight->user_id = $booking->user_id;
         $flight->status = self::FLIGHT_STATUS_STARTED;
-
         $flight->first_seen = gmdate('Y-m-d H:i:s');
-
         $flight = $this->updateData($flight);
-
         if ($flight->save()) {
             $booking->status = 2;
             $booking->save();
@@ -164,7 +166,7 @@ class ParseController extends Controller
      */
     private function endFlight($flight)
     {
-        $booking = Booking::find()->andWhere(['id' => $flight->booking_id]);
+        $booking = Booking::find()->andWhere(['id' => $flight->booking_id])->one();
         $booking->delete();
         if ($this->validateFlight($flight)) {
             $flight->lastseen = gmdate('Y-m-d H:i:s');
@@ -212,26 +214,30 @@ class ParseController extends Controller
     private function updateData($flight)
     {
         $data = $this->ourpilots[$flight->user_id];
-
+        $booking = Booking::find()->andWhere(['id' => $flight->booking_id])->one();
         $flight->from_icao = $booking->from_icao;
         $flight->to_icao = $booking->to_icao;
         $flight->lastseen = gmdate('Y-m-d H:i:s');
         $flight->flightplan = $data[self::WZ_FLIGHTPLAN];
-        $flight->fob = $data[self::WZ_FOB_HOURS].$data[self::WZ_FOB_MINUTES];
+        $flight->remarks = $data[self::WZ_REMARKS];
+        $flight->fob = $data[self::WZ_FOB_HOURS] . $data[self::WZ_FOB_MINUTES];
         $flight->pob = $data[self::WZ_POB];
-
+        $flight->domestic = $this->isDomestic($flight) ? 1 : 0;
+        $flight->alternate1 = $data[self::WZ_ALTERNATE];
+        $flight->nm = intval(Helper::calculateDistanceLatLng($flight->depairport->lat,$flight->arrairport->lat,$flight->depairport->lon,$flight->arraiport->lon));
+        $flight->sim = $data[self::WZ_SIMULATOR]; //according to ivao specifications (8-FS9, 9-FSX, 11-14 X-planes...)
         $this->insertTrackerData($flight);
-
         return $flight;
     }
 
     /**
      * Возвращает маршрутную часть ФПЛ.
      * @param int $int user_id для поиска в массиве с пилотами
+     * @return string
      */
-    private function getRoute($user_id)
+    private function getFlightRoute($user_id)
     {
-        return $this->ourpilots[$user_id][self::WZ_FPL_SPD] . $this->ourpilots[$user_id][self::WZ_FPL_ALT] . " " . $this->ourpilots[$user_id][self::WZ_FLIGHTPLAN]
+        return $this->ourpilots[$user_id][self::WZ_FPL_SPD] . $this->ourpilots[$user_id][self::WZ_FPL_ALT] . " " . $this->ourpilots[$user_id][self::WZ_FLIGHTPLAN];
     }
 
     /**
@@ -282,5 +288,13 @@ class ParseController extends Controller
             $this->ourpilots[$data[self::WZ_VID]] = $data;
             $this->onlinepilotslist[] = $data[self::WZ_VID];
         }
+    }
+
+    private function isDomestic($flight)
+    {
+        if ($flight->depairport->country == 'ru' && $flight->arrairport->country == 'ru') {
+            return true;
+        }
+        return false;
     }
 }
