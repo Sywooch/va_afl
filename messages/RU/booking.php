7721e2977c0f140422e3435161afcbedc730329d
Александр Шахмухаметов <diablus2012@gmail.com>
2016-01-06 01:05:56 +0500

    Букинг

---
+++ messages/RU/booking.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: BTH
+ * Date: 05.01.16
+ * Time: 22:20
+ */
+ return [
+     'Callsign' => 'Позывной',
+     'Departure airport' => 'Аэропорт вылета',
+     'Arrival Airport' => 'Аэропорт прилета',
+     'Aircraft Type' => 'Тип самолета (ICAO)',
+     'Aircraft Registration Number' => 'Регистрационный номер борта',
+     'Book' => 'Забронировать',
+     'Delete' => 'Удалить заявку',
+     'Ready' => 'Подана',
+     'In progress' => 'Выполняется',
+     'Booking status' => 'Состояние заявки',
+     'Available flights in schedule' => 'Доступные рейсы по расписанию',
+     'Booking details' => 'Детали бронирования',
+];
