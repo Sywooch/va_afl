06b71a921ed3fd77cbe7d6ba26ac67b0927c7d11
leo141198 <leo@sochi.com>
2016-01-09 21:19:57 +0300

    Fix

--- modules/pilot/views/default/edit.php
+++ modules/pilot/views/default/edit.php
@@ -5,7 +5,8 @@ $this->title = Yii::t('app', 'Profile editor');
 $this->params['breadcrumbs'] = [
     ['label' => Yii::t('app', 'Pilot'), 'url' => '/pilot'],
     ['label' => $this->title]
-]; ?>
+];
+$this->registerJs('function submitform() {$(\'#profile_edit\').submit();}') ?>
 <div class="edit-form">
     <div class="panel panel-inverse">
         <div class="panel-heading">
