032c21a5064f2581287b56f16c8c5630e732e076
leo141198 <leo@sochi.com>
2016-01-12 22:17:59 +0300

    Работает вступление в отряд

--- modules/squadron/views/default/view.php
+++ modules/squadron/views/default/view.php
@@ -4,33 +4,25 @@ use yii\helpers\Html;
 use yii\widgets\DetailView;
 use yii\grid\GridView;
 use yii\widgets\Pjax;
+use yii\helpers\Url;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Squadrons */
 
-$this->title = Yii::$app->language == 'RU' ? $squad->name_ru : $squad->name_en;
+$this->title = Yii::$app->language == 'RU' ? $squadron->name_ru : $squadron->name_en;
 $this->params['breadcrumbs'][] = ['label' => 'Squads', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="squads-view">
-    <div class="panel panel-inverse">
-        <div class="panel-body">
-            <h1><?= Html::encode($this->title) ?></h1>
+    <h1 class="page-header"><?= Html::encode($this->title) ?></h1>
 
-
-            <?= DetailView::widget([
-                'model' => $squad,
-                'attributes' => [
-                    'id',
-                    'name_ru',
-                    'name_en',
-                    'abbr_ru',
-                    'abbr_en',
-                ],
-            ]) ?>
-            <div class="row">
-                <div class="col-md-3">
-                    <h2 style="text-align: center">Список пилотов</h2>
+    <div class="row">
+        <div class="col-md-3">
+            <div class="panel panel-inverse">
+                <div class="panel-heading">
+                    <h4 class="panel-title">Список пилотов</h4>
+                </div>
+                <div class="panel-body">
                     <?php Pjax::begin() ?>
                     <?= GridView::widget([
                         'dataProvider' => $membersProvider,
@@ -39,12 +31,93 @@ $this->params['breadcrumbs'][] = $this->title;
                                 'attribute' => 'member_name',
                                 'label' => 'Member Name',
                                 'value' => function ($data) {
-                                    return $data->squadMember->full_name;
+                                    return $data->user->full_name;
                                 }
                             ]
                         ],
                     ]); ?>
                     <?php Pjax::end() ?>
+                    <?= Html::a('Отправить заявку на вступление', Url::to(['join', 'squadron' => $squadron->id]),['class' => 'btn btn-primary'])?>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-9">
+            <div class="panel panel-inverse">
+                <div class="panel-heading">
+                    <h4 class="panel-title">Информация</h4>
+                </div>
+                <div class="panel-body">
+                    <ul class="nav nav-tabs">
+                        <li class="active"><a href="#default-tab-1" data-toggle="tab" aria-expanded="true">Default Tab
+                                1</a>
+                        </li>
+                        <li class=""><a href="#default-tab-2" data-toggle="tab" aria-expanded="false">Default Tab 2</a>
+                        </li>
+                        <li class=""><a href="#default-tab-3" data-toggle="tab" aria-expanded="false">Default Tab 3</a>
+                        </li>
+                    </ul>
+                    <div class="tab-content">
+                        <div class="tab-pane fade active in" id="default-tab-1">
+                            <p>
+                                <img
+                                    src="http://samolets.com/wp-content/gallery/boeing-737-800-aeroflot/boeing-737-800-vp-brf-aeroflot-3.jpg" height="400">
+                                <br>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Integer ac dui eu felis hendrerit lobortis. Phasellus elementum, nibh eget
+                                adipiscing
+                                porttitor,
+                                est diam sagittis orci, a ornare nisi quam elementum tortor. Proin interdum ante
+                                porta
+                                est
+                                convallis
+                                dapibus dictum in nibh. Aenean quis massa congue metus mollis fermentum eget et
+                                tellus.
+                                Aenean tincidunt, mauris ut dignissim lacinia, nisi urna consectetur sapien, nec
+                                eleifend orci
+                                eros id lectus.
+                            </p>
+                            <p class="text-right m-b-0">
+                                <a href="javascript:;" class="btn btn-white m-r-5">Default</a>
+                                <a href="javascript:;" class="btn btn-primary">Primary</a>
+                            </p>
+                        </div>
+                        <div class="tab-pane fade" id="default-tab-2">
+                            <blockquote>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                <small>Someone famous in <cite title="Source Title">Source Title</cite></small>
+                            </blockquote>
+                            <h4>Lorem ipsum dolor sit amet</h4>
+                            <p>
+                                Nullam ac sapien justo. Nam augue mauris, malesuada non magna sed, feugiat blandit
+                                ligula.
+                                In tristique tincidunt purus id iaculis. Pellentesque volutpat tortor a mauris
+                                convallis,
+                                sit amet scelerisque lectus adipiscing.
+                            </p>
+                        </div>
+                        <div class="tab-pane fade" id="default-tab-3">
+                            <p>
+								<span class="fa-stack fa-4x pull-left m-r-10">
+									<i class="fa fa-square-o fa-stack-2x"></i>
+									<i class="fa fa-twitter fa-stack-1x"></i>
+								</span>
+                                Praesent tincidunt nulla ut elit vestibulum viverra. Sed placerat magna eget eros
+                                accumsan
+                                elementum.
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam quis lobortis neque.
+                                Maecenas justo odio, bibendum fringilla quam nec, commodo rutrum quam.
+                                Donec cursus erat in lacus congue sodales. Nunc bibendum id augue sit amet placerat.
+                                Quisque et quam id felis tempus volutpat at at diam. Vivamus ac diam turpis.Sed at
+                                lacinia
+                                augue.
+                                Nulla facilisi. Fusce at erat suscipit, dapibus elit quis, luctus nulla.
+                                Quisque adipiscing dui nec orci fermentum blandit.
+                                Sed at lacinia augue. Nulla facilisi. Fusce at erat suscipit, dapibus elit quis, luctus
+                                nulla.
+                                Quisque adipiscing dui nec orci fermentum blandit.
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
