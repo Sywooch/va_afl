527afd5ece35831f0ae0e8b4139ca59730edcb34
Александр Шахмухаметов <diablus2012@gmail.com>
2016-02-13 20:43:42 +0500

    Исправлена ошибка логики нагружателя паксов.

--- commands/ParseController.php
+++ commands/ParseController.php
@@ -140,13 +140,13 @@ class ParseController extends Controller
         if ($booking->fleet_regnum) {
             $flight->fleet_regnum = $booking->fleet_regnum;
         }
-        $paxs=Pax::appendPax($flight->from_icao,$flight->to_icao,$flight->fleet,true);
         $flight->acf_type = Fleet::find()->andWhere(['id'=>$flight->fleet_regnum])->one()->type_code;
         $flight->booking_id = $booking->id;
         $flight->user_id = $booking->user_id;
         $flight->status = self::FLIGHT_STATUS_STARTED;
         $flight->first_seen = gmdate('Y-m-d H:i:s');
         $flight = $this->updateData($flight);
+        $paxs=Pax::appendPax($flight->from_icao,$flight->to_icao,$flight->fleet,true);
         $flight->pob = $paxs['total'];
         $flight->vucs = Billing::calculatePriceForFlight($flight->from_icao,$flight->to_icao,$paxs['paxtypes']);
         if ($flight->save()) {
