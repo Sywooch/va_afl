d609ef66cbba680b26bbfd1173f2a6d85122154c
Alexander Shakhmukhametov <diablus2012@gmail.com>
2016-02-19 15:01:53 +0100

    Update

--- config/params.php
+++ config/params.php
@@ -4,7 +4,7 @@ return [
     'adminEmail' => 'admin@example.com',
     'ivao_login_url' => 'https://login.ivao.aero/index.php?url=http://' . $_SERVER['HTTP_HOST'] . '/site/login',
     'ivao_api_url' => 'https://login.ivao.aero/api.php?type=json&token=',
-    'ivaoru_api_url' => 'http://ivaoru.org/api/futureevents?key=975b7d632cb80cdb7f1145006573ee9f',
     'whitelist' => [439914, 422747, 442121, 464736, 405896, 473695, 513039],
+    'ivaoru_api_url' => 'http://ivaoru.org/api/futureevents?key=975b7d632cb80cdb7f1145006573ee9f',
     'slack' => 'https://hooks.slack.com/services/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'
 ];
