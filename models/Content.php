0210640a097e3d1be6d9521503b8e93743fbf4ab
Nikita Fedoseev <agent.daitel@gmail.com>
2016-02-23 01:51:01 +0300

    Исправление модели content для работы скриншотов Дизайн таймлайна

--- models/Content.php
+++ models/Content.php
@@ -39,7 +39,7 @@ class Content extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['category', 'name_ru', 'name_en', 'text_ru', 'text_en'], 'required'],
+            [['category', 'name_ru', 'name_en'], 'required'],
             [['category', 'author'], 'integer'],
             [['text_ru', 'text_en'], 'string'],
             [['created'], 'safe'],
