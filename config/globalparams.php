af4cca642d68cd158f951cd7f3c9dc9bd0af4cff
Nikita Fedoseev <agent.daitel@gmail.com>
2016-02-24 21:34:53 +0300

    fix

--- config/globalparams.php
+++ config/globalparams.php
@@ -9,5 +9,6 @@
 return [
     'whitelist' => [439914, 422747, 442121, 464736, 405896, 473695, 513039],
     'ivaoru_api_url' => 'http://ivaoru.org/api/futureevents?key=975b7d632cb80cdb7f1145006573ee9f',
+    'ivao_rss' => 'https://www.ivao.aero/EVENTS/rss.asp',
     'slack' => 'https://hooks.slack.com/services/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'
 ];
