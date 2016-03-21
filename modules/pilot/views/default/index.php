07bb2014c7f53320357799e63ab2479df2b53d95
leo141198 <leo@sochi.com>
2016-03-21 19:31:06 +0300

    Initial commit

--- modules/pilot/views/default/index.php
+++ modules/pilot/views/default/index.php
@@ -1,36 +1,29 @@
 <?php
-use app\assets\MapAsset;
-use app\assets\StatisticAsset;
-
 use yii\helpers\Html;
-use yii\widgets\DetailView;
+use yii\helpers\Url;
 use yii\grid\GridView;
 
-MapAsset::register($this);
-StatisticAsset::register($this);
+/**
+ * Created by PhpStorm.
+ * User: BTH
+ * Date: 23.09.15
+ * Time: 20:32
+ */
 
-
-/* @var $this yii\web\View */
-
-$this->title = Yii::t('app', 'My Statistics');
+$this->title = Yii::t('app', 'Statistics');
 $this->params['breadcrumbs'] = [
     ['label' => Yii::t('app', 'Pilot Center'), 'url' => '/pilot/center'],
     ['label' => $this->title]
-];
-
-?>
-
-<h1><?= Html::encode($this->title) ?></h1>
+]; ?>
 <div class="panel panel-inverse">
     <div class="panel-heading">
-        <h4 class="panel-title">Map</h4>
+        <h4 class="panel-title"><?= $this->title ?></h4>
     </div>
-    <div class="panel-body">
-        <div id="map" style="width: 1000px; height: 670px;"></div>
-        <script>
-            setTimeout(function () {
-                initialize(<?= Yii::$app->user->identity->vid ?>);
-            }, 500);
-        </script>
+    <div class="panel-body" style="display: block;">
+        <div class="raw">
+            <div class="col md-6">
+                
+            </div>
+        </div>
     </div>
 </div>
