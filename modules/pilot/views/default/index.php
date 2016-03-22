a6658e2af1aacc487fd6976324149841741045d8
leo141198 <leo@sochi.com>
2016-03-22 22:16:27 +0300

    fix

--- modules/pilot/views/default/index.php
+++ modules/pilot/views/default/index.php
@@ -16,7 +16,6 @@ $this->params['breadcrumbs'] = [
     </div>
     <div class="panel-body" style="display: block;">
         <div class="raw">
-            <?php BaseVarDumper::dump($user->pilot->statFlightsRegular, '10', true); ?>
             <div class="col-md-3">
                 <?php
                 echo HighCharts::widget(
@@ -168,6 +167,81 @@ $this->params['breadcrumbs'] = [
                                     'name' => 'Types',
                                     'colorByPoint' => true,
                                     'data' => $user->pilot->statAcfTypes,
+                                    'innerSize' => '65%'
+                                ]
+                            ]
+                        ]
+                    ]
+                ); ?>
+            </div>
+            <div class="col-md-3">
+                <?php echo HighCharts::widget(
+                    [
+                        'clientOptions' => [
+                            'colors' => ['#F59C1A', '#FF5B57', '#B6C2C9', '#2D353C', '#348FE2'],
+                            'chart' => [
+                                'type' => 'pie',
+                                'plotBackgroundColor' => null,
+                                'backgroundColor' => null,
+                                'plotBorderWidth' => null,
+                                'plotShadow' => false,
+                                'height' => 300,
+                                'style' => [
+                                    'fontFamily' => 'Open Sans',
+                                    'fontSize' => '12px',
+                                    'color' => '#777777',
+                                    'fontWeight' => '600',
+                                ]
+                            ],
+                            'title' => [
+                                'text' => Yii::t('charts', 'FLIGHT TYPES'),
+                                'style' => [
+                                    'fontFamily' => 'Open Sans',
+                                    'fontSize' => '12px',
+                                    'color' => '#777777',
+                                    'fontWeight' => '600',
+                                    'margin-top' => 20,
+                                ]
+                            ],
+                            'exporting' => [
+                                'enabled' => false
+                            ],
+                            'credits' => [
+                                'enabled' => false
+                            ],
+                            'tooltip' => [
+                                'pointFormat' => '{series.name}: <b>{point.percentage:.1f}%</b>',
+                                'style' => [
+                                    'fontFamily' => 'Open Sans',
+                                    'fontSize' => '12px',
+                                    'color' => '#777777',
+                                    'fontWeight' => '600'
+                                ]
+                            ],
+                            'plotOptions' => [
+                                'pie' => [
+                                    'allowPointSelect' => true,
+                                    'cursor' => 'pointer',
+                                    'dataLabels' => [
+                                        'enabled' => false
+                                    ],
+                                    'showInLegend' => true,
+                                ]
+                            ],
+                            'legend' => [
+                                'itemStyle' => [
+                                    'fontFamily' => 'Open Sans',
+                                    'fontSize' => '12px',
+                                    'color' => '#777777',
+                                    'fontWeight' => '600',
+                                ],
+                                'borderColor' => '#FFFFFF',
+                            ],
+                            'series' => [
+                                [
+                                    'name' => 'Types',
+                                    'colorByPoint' => true,
+                                    'data' => $user->pilot->statFlightsDomestic,
                                     'innerSize' => '65%'
                                 ]
                             ]
