be5b0b5ff84bd6c478f83c74d98441e3f3b4e087
Nikita Fedoseev <agent.daitel@gmail.com>
2016-05-11 20:03:23 +0300

    Герман Поляк и Роман Савин - доступ Апи для эвентов

---
+++ controllers/ApiController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace app\controllers;
+
+use app\components\Helper;
+use app\models\Billing;
+use app\models\BillingPayments;
+use app\models\BillingUserBalance;
+use app\models\UserPilot;
+use Yii;
+use yii\db\Query;
+use yii\web\Controller;
+use yii\web\Response;
+use yii\web\User;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+use yii\data\ActiveDataProvider;
+use yii\helpers\Url;
+use app\commands\ParseController;
+
+use app\models\Events\Events;
+use app\models\Events\Calendar;
+
+/**
+ * Class SiteController
+ * @package app\controllers
+ */
+class ApiController extends Controller
+{
+    /**
+     * @return array
+     */
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+    public function actionEvents()
+    {
+        echo json_encode(
+            [
+                'events' => Events::center(true),
+                'eventsCalendar' => Calendar::center()
+            ]
+        );
+    }
+}
