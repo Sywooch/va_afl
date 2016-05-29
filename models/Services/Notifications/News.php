75b2ebd24a5f4787834a987ecf8ef084e0bc1cdc
Nikita Fedoseev <agent.daitel@gmail.com>
2016-05-29 21:24:41 +0300

    notific

---
+++ models/Services/Notifications/News.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nikita Fedoseev
+ * Date: 28.05.16
+ * Time: 19:30
+ */
+
+namespace app\models\Services\Notifications;
+
+use Yii;
+
+use app\models\Users;
+
+class News
+{
+    /**
+     * @param $content \app\models\Content
+     */
+    public static function add($content)
+    {
+        foreach(Users::active() as $user){
+            if(Yii::$app->authManager->checkAccess($user->vid, $content->categoryInfo->access_feed)){
+                Notification::add($user->vid, $content->author, $content->id);
+
+                Yii::$app->mailer->compose('notification_news.php', ['user' => $user, 'content' => $content])
+                    ->setFrom('noreply@va-transaero.ru')
+                    ->setTo($user->email)
+                    ->setSubject('AFL Group News '.$content->name_en)
+                    ->send();
+            }
+        }
+    }
+} 
---
+++ models/Services/Notifications/Notification.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace app\models\Services\Notifications;
+
+use Yii;
+
+use app\models\Users;
+use app\models\Content;
+
+/**
+ * This is the model class for table "{{%notification}}".
+ *
+ * @property integer $id
+ * @property integer $to
+ * @property integer $from
+ * @property integer $content_id
+ * @property integer $read
+ * @property string $created
+ * @property string $icon
+ * @property string $color
+ * @property string $link
+ *
+ * @property Users $toUser
+ * @property Users $fromUser
+ * @property Content $content
+ */
+class Notification extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return '{{%notification}}';
+    }
+
+    public static function add($vid, $author, $id)
+    {
+        $notification = new Notification;
+        $notification->to = $vid;
+        $notification->from = $author;
+        $notification->content_id = $id;
+        $notification->save();
+    }
+
+    public static function last()
+    {
+        return self::user()->orderBy('created desc')->limit(5)->all();
+    }
+
+    public static function all()
+    {
+        return self::user()->orderBy('created desc')->all();
+    }
+
+
+    public static function count()
+    {
+        return self::user()
+            ->count();
+    }
+
+    public static function user()
+    {
+        return self::find()->where(['to' => Yii::$app->user->identity->vid])->andWhere(['read' => 0]);
+    }
+
+    public function getIconHTML(){
+        return $this->icon ? '<i class="fa '.$this->icon.' media-object bg-'.$this->color.'"></i>': '<img src="'.$this->fromUser->avatarLink.'" class="media-object" alt=""/>';
+    }
+
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['to', 'from', 'content_id'], 'required'],
+            [['to', 'from', 'content_id', 'read'], 'integer'],
+            [['created'], 'safe'],
+            [['icon', 'color', 'link'], 'string', 'max' => 50]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'to' => Yii::t('app', 'To'),
+            'from' => Yii::t('app', 'From'),
+            'content_id' => Yii::t('app', 'Content ID'),
+            'read' => Yii::t('app', 'Read'),
+            'created' => Yii::t('app', 'Created'),
+            'tag' => Yii::t('app', 'Tag'),
+            'tag_color' => Yii::t('app', 'Tag Color'),
+            'link' => Yii::t('app', 'Link'),
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getToUser()
+    {
+        return $this->hasOne(Users::className(), ['vid' => 'to']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getFromUser()
+    {
+        return $this->hasOne(Users::className(), ['vid' => 'from']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getContent()
+    {
+        return $this->hasOne(Content::className(), ['id' => 'content_id']);
+    }
+}
