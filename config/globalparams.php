06879bda6f1fa61b4d5d0639f09c73341965249b
leo141198 <leo@sochi.com>
2016-07-07 17:01:47 +0300

    Fix правил + список бета-тестеров

--- config/globalparams.php
+++ config/globalparams.php
@@ -9,29 +9,30 @@
 return [
 
     'whitelist' => [
-        489335,
-        439914,
-        422747,
-        442121,
-        464736,
-        405896,
-        473695,
-        513039,
-        476544,
-        477336,
-        505004,
-        517147,
-        495457,
-        482834,
-        482390,
-        407230,
-        508117,
-        348350,
-        357904,
-        412370,
-        413482,
-        505105,
-        508469
+        489335, //ANDREY LUN
+        439914, //ALEXANDER SHAKHMUKHAMETOV
+        422747, //NIKITA FEDOSEEV
+        442121, //MIKHAIL IDELCHIK
+        464736, //LEONID KUTSENOK
+        405896, //SERGEY MOVCHAN
+        473695, //ANDREI ISLEVSKI
+        513039, //ANNA IVOYLOVA
+        476544, //VIKTOR SIKORSKII
+        477336, //GERMAN POLYAK
+        505004, //ROMAN SAVIN
+        517147, //VLADISLAV BOZIYAN
+        495457, //EVGENY MINENKOV
+        482834, //ALEXANDR KHVOSTOV
+        482390, //IVAN SAPRONOV
+        407230, //RUSTAM KILICHEV
+        508117, //DMITRY LAVROV
+        348350, //FERNANDO C PALACIO
+        357904, //ALEXANDER. IVANOV
+        412370, //NIKOLAY ISHENKO
+        413482, //KIRILL RASSOLENKO
+        505105, //KIRILL MOSIN
+        508469, //SERGEY EGOROV
+        340167, //MAXIM FOKEEV
     ],
     'ivaoru_api_url' => 'http://ivaoru.org/api/futureevents?key=975b7d632cb80cdb7f1145006573ee9f',
     'ivao_rss' => 'https://www.ivao.aero/EVENTS/rss.asp',
