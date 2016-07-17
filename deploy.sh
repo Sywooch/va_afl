e8fee7d982e9bd9c64f60d1435ae52d1ad5ebe1a
Nikita Fedoseev <agent.daitel@gmail.com>
2016-07-17 17:13:08 +0300

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 if [[ -d .git ]]; then
-    
+    echo ".git folder exist"
 else
+    echo "init git"
     git init
     git remote add origin $1
 fi
 
+echo "get repo"
 git reset —hard
 git pull origin $2
 
+echo "composers tasks"
 composer install
 composer global require "fxp/composer-asset-plugin:^1.2.0"
 composer update
