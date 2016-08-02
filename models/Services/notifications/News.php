c3bba459262af6a4f367ae9907504ea4d5e38527
Nikita Fedoseev <agent.daitel@gmail.com>
2016-08-02 13:38:44 +0300

    news mailer on

--- models/Services/notifications/News.php
+++ models/Services/notifications/News.php
@@ -23,11 +23,11 @@ class News
             if(Yii::$app->authManager->checkAccess($user->vid, $content->categoryInfo->access_feed)){
                 Notification::add($user->vid, $content->author, $content->id);
 
-               /* Yii::$app->mailer->compose('notification_news.php', ['user' => $user, 'content' => $content])
+                Yii::$app->mailer->compose('notification_news.php', ['user' => $user, 'content' => $content])
                     ->setFrom('noreply@va-transaero.ru')
                     ->setTo($user->email)
                     ->setSubject('AFL Group News '.$content->name_en)
-                    ->send();*/
+                    ->send();
             }
         }
     }
