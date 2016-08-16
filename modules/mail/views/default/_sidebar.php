d8375fb53d4524abcc3eda104a676fb7e3f6a272
Nikita Fedoseev <agent.daitel@gmail.com>
2016-08-16 21:19:06 +0300

    Модуль новости

---
+++ modules/mail/views/default/_sidebar.php
@@ -0,0 +1,20 @@
+<div class="vertical-box-column width-250">
+    <!-- begin wrapper -->
+    <div class="wrapper bg-silver text-center">
+        <a href="email_compose.html" class="btn btn-success p-l-40 p-r-40 btn-sm">
+            Compose
+        </a>
+    </div>
+    <!-- end wrapper -->
+    <!-- begin wrapper -->
+    <div class="wrapper">
+        <p><b>FOLDERS</b></p>
+        <ul class="nav nav-pills nav-stacked nav-sm">
+            <li class="active"><a href="email_inbox_v2.html"><i class="fa fa-inbox fa-fw m-r-5"></i> Inbox <span class="badge pull-right">52</span></a></li>
+            <li><a href="email_inbox_v2.html"><i class="fa fa-flag fa-fw m-r-5"></i> Important</a></li>
+            <li><a href="email_inbox_v2.html"><i class="fa fa-send fa-fw m-r-5"></i> Sent</a></li>
+            <li><a href="email_inbox_v2.html"><i class="fa fa-pencil fa-fw m-r-5"></i> Drafts</a></li>
+            <li><a href="email_inbox_v2.html"><i class="fa fa-trash fa-fw m-r-5"></i> Trash</a></li>
+        </ul>
+    </div>
+</div>
---
+++ modules/mail/views/default/_top.php
@@ -0,0 +1,25 @@
+<div class="wrapper bg-silver-lighter">
+    <div class="btn-toolbar">
+        <div class="btn-group pull-right">
+            <button class="btn btn-white btn-sm">
+                <i class="fa fa-chevron-left"></i>
+            </button>
+            <button class="btn btn-white btn-sm">
+                <i class="fa fa-chevron-right"></i>
+            </button>
+        </div>
+        <div class="btn-group dropdown">
+            <button class="btn btn-white btn-sm dropdown-toggle" data-toggle="dropdown">
+                View All <span class="caret"></span>
+            </button>
+            <ul class="dropdown-menu text-left text-sm">
+                <li class="active"><a href="#"><i class="fa fa-circle f-s-10 fa-fw m-r-5"></i> All</a></li>
+                <li><a href="javascript:;"><i class="fa f-s-10 fa-fw m-r-5"></i> Unread</a></li>
+            </ul>
+        </div>
+        <div class="btn-group">
+            <button class="btn btn-sm btn-white" data-toggle="tooltip" data-placement="top" data-title="Refresh"
+                    data-original-title="" title=""><i class="fa fa-refresh"></i></button>
+        </div>
+    </div>
+</div>
