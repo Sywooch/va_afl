b1ccf3d5a189ce3d35f3115da0c2da9e8f1611c7
Nikita Fedoseev <agent.daitel@gmail.com>
2016-08-17 16:23:10 +0300

    Новости под релиз

--- mail/notification_news.php
+++ mail/notification_news.php
@@ -25,7 +25,7 @@ use yii\helpers\Html;
                             <table class="btn-primary" cellpadding="0" cellspacing="0" border="0">
                                 <tr>
                                     <td>
-                                        <a href="<?= Yii::$app->urlManager->baseUrl ?>/<?= $link ?>">View more info</a>
+                                        <a href="http://va-afl.su/<?= $link ?>">View more info</a>
                                     </td>
                                 </tr>
                             </table>
