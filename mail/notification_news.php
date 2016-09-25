3bcda6265619c28deff90d350bad6b85217b91d9
Nikita Fedoseev <agent.daitel@gmail.com>
2016-09-25 16:29:59 +0300

    hotfix

--- mail/notification_news.php
+++ mail/notification_news.php
@@ -9,6 +9,7 @@
 use yii\helpers\Html;
 
 ?>
+<?php if($user->language == 'EN'): ?>
 <table class="body-wrap" bgcolor="#f6f6f6">
     <tr>
         <td></td>
@@ -62,3 +63,55 @@ use yii\helpers\Html;
         <td></td>
     </tr>
 </table>
+<?php else:?>
+<table class="body-wrap" bgcolor="#f6f6f6">
+    <tr>
+        <td></td>
+        <td class="container" bgcolor="#FFFFFF">
+            <div class="content">
+                <table>
+                    <tr>
+                        <td>
+                            <h1>Привет, <?= Html::encode($user->full_name) ?>!</h1>
+
+                            <h2><?= $content->name_ru ?></h2>
+
+                            <p><?= $content->description_ru ?></p>
+                            <table class="btn-primary" cellpadding="0" cellspacing="0" border="0">
+                                <tr>
+                                    <td>
+                                        <a href="http://va-afl.su/<?= $link ?>?utm_source=notificationemail&utm_medium=email&utm_content=bluebutton&utm_campaign=none">View more info</a>
+                                    </td>
+                                </tr>
+                            </table>
+                            <p>Безопасных полётов с нами,</p>
+                            <p>VA AFL Team</p>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </td>
+        <td></td>
+    </tr>
+</table>
+<table class="footer-wrap">
+    <tr>
+        <td></td>
+        <td class="container">
+            <div class="content">
+                <table>
+                    <tr>
+                        <td align="center">
+                            <p>Не хотите получать такие письма? Напишите на <a href="mailto:support@va-afl.su">
+                                    support@va-afl.su
+                                </a>.
+                            </p>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </td>
+        <td></td>
+    </tr>
+</table>
+<?php endif; ?>
