cc043ae99240bd322879988dc2cee42d9a656ff7
Nikita Fedoseev <agent.daitel@gmail.com>
2016-10-01 00:08:17 +0300

    delete button for fleet

--- modules/airline/views/fleet/view.php
+++ modules/airline/views/fleet/view.php
@@ -19,6 +19,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php if (Yii::$app->user->can('fleet/edit')): ?>
         <p>
             <?= Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+            <?= Html::a(Yii::t('app', 'Delete'), ['delete', 'id' => $model->id], ['class' => 'btn btn-danger']) ?>
         </p>
     <?php endif; ?>
 
