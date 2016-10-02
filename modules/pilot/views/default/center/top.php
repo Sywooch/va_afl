82ba25f72f0b31b96ec3401c24e824c42314a17d
Nikita Fedoseev <agent.daitel@gmail.com>
2016-10-02 20:55:31 +0300

    другой

--- modules/pilot/views/default/center/top.php
+++ modules/pilot/views/default/center/top.php
@@ -11,30 +11,65 @@ use yii\helpers\Html;
 use yii\helpers\Url;
 use yii\grid\GridView;
 use yii\widgets\Pjax;
+
 ?>
 <!-- begin panel -->
 <div class="panel panel-inverse">
     <div class="panel-heading">
         <h4 class="panel-title"><?= Yii::t('app', 'Top') . ' ' . Yii::t('top', 'by mouth') ?></h4>
     </div>
-    <div class="panel-body bg-silver">
+    <div class="panel-body">
         <?php Pjax::begin(); ?>
-        <?= GridView::widget([
-            'dataProvider' => $dataProvider,
-            'columns' => [
-                [
-                    'attribute' => 'user_id',
-                    'format' => 'raw',
-                    'value' => function ($data) {
-                        return "<img src=" . $data->user->flaglink . "> " .
-                        Html::a($data->user->full_name, Url::to('/pilot/profile/' . $data->user->vid),
-                            ['target' => '_blank']);
-                    }
+        <?=
+        GridView::widget(
+            [
+                'dataProvider' => $dataProvider,
+                'layout' => '{items}{pager}',
+                'options' => ['class' => 'table table-condensed table-responsive'],
+                'columns' => [
+                    [
+                        'label' => Yii::t('app', 'Pilot'),
+                        'attribute' => 'user_id',
+                        'format' => 'raw',
+                        'value' => function ($data) {
+                                return "<img src=" . $data->user->flaglink . "> " .
+                                Html::a(
+                                    $data->user->full_name,
+                                    Url::to('/pilot/profile/' . $data->user->vid),
+                                    ['target' => '_blank']
+                                );
+                            }
+                    ],
+                    [
+                        'label' => Yii::t('app', 'Location'),
+                        'attribute' => 'user.location',
+                        'format' => 'raw',
+                        'value' => function ($data) {
+                                return '<img src="' . $data->user->pilot->airport->flaglink . '"> ' . Html::a(
+                                    Html::encode(
+                                        $data->user->pilot->airport->name . ' (' . $data->user->pilot->location . ')'
+                                    ),
+                                    Url::to(
+                                        [
+                                            '/airline/airports/view/',
+                                            'id' => $data->user->pilot->location
+                                        ]
+                                    ),
+                                    [
+                                        'data-toggle' => "tooltip",
+                                        'data-placement' => "top",
+                                        'title' => Html::encode(
+                                                "{$data->user->pilot->airport->city}, {$data->user->pilot->airport->iso}"
+                                            )
+                                    ]
+                                );
+                            }
+                    ],
+                    'rating_pos',
+                    //'rating_count',
                 ],
-                'rating_pos',
-                'rating_count',
-            ],
-        ]); ?>
+            ]
+        ); ?>
         <?php Pjax::end(); ?>
     </div>
 </div>
