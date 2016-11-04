3b6700437e56028a3c0121186823c49010995e2f
Nikita Fedoseev <agent.daitel@gmail.com>
2016-11-04 22:56:30 +0300

    rating update

--- commands/TopController.php
+++ commands/TopController.php
@@ -1,21 +1,16 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: BTH
- * Date: 06.01.16
- * Time: 14:40
- *
- * Запуск из крона каждый час
- *
+ * Запуск из крона раз в день
  */
 namespace app\commands;
+
+use yii\console\Controller;
+use yii\helpers\ArrayHelper;
 
 use app\models\Top\PositionSet;
 use app\models\Top\Rating;
 use app\models\Top\Top;
 use app\models\UserPilot;
-use yii\console\Controller;
-use yii\helpers\ArrayHelper;
 
 class TopController extends Controller
 {
@@ -28,28 +23,27 @@ class TopController extends Controller
     public function actionRecollect(){
         for ($year = 2015; $year <= gmdate("Y"); $year++){
             for($mouth = 1; $mouth <= 12; $mouth++){
-                $this->stats($mouth, $year);
-                $this->pos($mouth, $year);
-                $this->rating($mouth, $year);
-                $this->pos($mouth, $year, true);
+                $this->process($mouth, $year);
             }
         }
     }
 
     private function all()
     {
-        $this->stats(0, 0);
-        $this->pos(0, 0);
-        $this->rating(0, 0);
-        $this->pos(0, 0, true);
+        $this->process(0,0);
     }
 
     private function currentMouth()
     {
-        $this->stats(gmdate("m"), gmdate("Y"));
-        $this->pos(gmdate("m"), gmdate("Y"));
-        $this->rating(gmdate("m"), gmdate("Y"));
-        $this->pos(gmdate("m"), gmdate("Y"), true);
+        $this->process(gmdate("m"), gmdate("Y"));
+    }
+    
+    private function process($mouth, $year)
+    {
+        $this->stats($mouth, $year);
+        $this->pos($mouth, $year);
+        $this->rating($mouth, $year);
+        $this->pos($mouth, $year, true);
     }
 
     private function stats($mouth, $year)
